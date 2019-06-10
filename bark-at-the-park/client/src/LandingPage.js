56f15fd5839e445d3a6a0acca32c0acd0cc0287d
jebediahelliott <jebediahelliott@gmail.com>
2019-06-10 19:08:47 -0400

    Add test and stub for LandingPage

---
+++ bark-at-the-park/client/src/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+
+
+  render() {
+    return (
+      <button>Sign Up</button>
+    )
+  }
+}
+
+
+export default LandingPage;
