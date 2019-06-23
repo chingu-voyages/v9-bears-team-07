f34155437adb45bafb955b40dcf2ac256a73a07b
jebediahelliott <jebediahelliott@gmail.com>
2019-06-22 21:53:23 -0400

    Stub out email login

--- bark-at-the-park/client/src/components/Login/Login.js
+++ bark-at-the-park/client/src/components/Login/Login.js
@@ -14,7 +14,9 @@ class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            logged: false
+            logged: false,
+            email: '',
+            password: ''
         }
     }
 
@@ -33,6 +35,25 @@ class Login extends Component {
 
     }
 
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+
+    handleEmailLogin = () => {
+      axios.post('http://localhost:3001/auth/email_login', {
+          email: this.state.email,
+          password: this.state.password
+      })
+      .then(res => {
+          console.log('res', res)
+          console.log('props',this.props)
+          // return this.props.history.push('/homepage')
+      })
+      .catch(err => console.err(err))
+    }
+
     render() {
         return (
             <div className="log-btn">
@@ -40,6 +61,11 @@ class Login extends Component {
                     buttonText="Logout"
                     onLogoutSuccess={this.logout}
                 /> */}
+                <form onSubmit={this.handleEmailLogin}>
+                  <input type="email" value={this.state.email} onChange={this.handleChange} id="email" />
+                  <input type="password" value={this.state.password} onChange={this.handleChange} id="password" />
+                  <input type="submit" id="submit" />
+                </form>
                 <GoogleLogin
                     clientId="315721689688-0kcdvhqs7f2p8u6nc35v63ovlf59c62d.apps.googleusercontent.com"
                     buttonText="Login"
