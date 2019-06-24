c757bdce5972a6b6ae4c90c9138d45226b8ec6c7
Gonpeche <espeche.g@gmail.com>
2019-06-24 00:25:11 -0300

    mapbox fixes

--- bark-at-the-park/client/src/components/Map/Map.js
+++ bark-at-the-park/client/src/components/Map/Map.js
@@ -1,30 +1,47 @@
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Mapa = ReactMapboxGl({
-    accessToken: "pk.eyJ1IjoieW9pemFsbyIsImEiOiJjang5cDhia3cweTZ1M3RydnJ3YWN3bGhqIn0.x4xiNokEzEVOMyyFFQ1SiQ"
-});
+export default function Map () {
+    const [location, setLocation] = useState([-58.2554584,-37.8464067])
 
+    const Mapa = ReactMapboxGl({
+        accessToken: "pk.eyJ1IjoieW9pemFsbyIsImEiOiJjang5cDhia3cweTZ1M3RydnJ3YWN3bGhqIn0.x4xiNokEzEVOMyyFFQ1SiQ"
+    });
 
-export default class Map extends Component {
-    render() {
-        return (
+    let error = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    let success = (pos) => {
+        let latitude = pos.coords.latitude;
+        let longitude = pos.coords.longitude;
+        let coordinate = [latitude, longitude]
+
+        setLocation(coordinate)    
+    }
+
+    useEffect( () => { 
+        navigator.geolocation.getCurrentPosition(success, error);}
+    );
+
+    return (
             <div>
                 <Mapa
-                style="mapbox://styles/mapbox/streets-v9"
-                center={[-58.417347, -34.5850838]}
-                containerStyle={{
-                    height: "50vh",
-                    width: "50vw",
+                    // eslint-disable-next-line react/style-prop-object
+                    style="mapbox://styles/mapbox/streets-v9"
+                    zoom={[13]}
+                    center={[location[1], location[0]]}
+                    containerStyle={{
+                        height: "50vh",
+                        width: "50vw",
                 }}>
                     <Layer
-                    type="symbol"
-                    id="marker"
-                    layout={{ "icon-image": "marker-15" }}>
+                        type="symbol"
+                        id="marker"
+                        layout={{ "icon-image": "marker-15" }}>
                         <Feature coordinates={[-58.417347, -34.5850838]}/>
                     </Layer>
                 </Mapa>
             </div>
         )
-    }
 }
