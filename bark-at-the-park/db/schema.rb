fd525bd086fbe5bf5808b76ac2c18a5b68a47854
jebediahelliott <jebediahelliott@gmail.com>
2019-06-25 22:06:39 -0400

    Update user model

--- bark-at-the-park/db/schema.rb
+++ bark-at-the-park/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_014624) do
+ActiveRecord::Schema.define(version: 2019_06_26_020121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,13 +43,15 @@ ActiveRecord::Schema.define(version: 2019_06_26_014624) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "logged_in", default: false
     t.boolean "google_user", default: false
+    t.string "last_name"
+    t.string "avatar"
   end
 
   add_foreign_key "dogs", "users"
