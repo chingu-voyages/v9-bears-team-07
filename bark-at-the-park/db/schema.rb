c5d276d7cbbd2f323d589299f0250d8b3b2954e7
jebediahelliott <jebediahelliott@gmail.com>
2019-06-25 22:16:05 -0400

    Update parks model

--- bark-at-the-park/db/schema.rb
+++ bark-at-the-park/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_020121) do
+ActiveRecord::Schema.define(version: 2019_06_26_021435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_06_26_020121) do
     t.time "closing_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "location", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
