11cd11d7223a39b03cea14797e2771383d7e41b9
jebediahelliott <jebediahelliott@gmail.com>
2019-06-25 21:49:46 -0400

    Update dog model

--- bark-at-the-park/db/schema.rb
+++ bark-at-the-park/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_003550) do
+ActiveRecord::Schema.define(version: 2019_06_26_014624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2019_06_15_003550) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
   create_table "park_users", force: :cascade do |t|
@@ -49,4 +52,5 @@ ActiveRecord::Schema.define(version: 2019_06_15_003550) do
     t.boolean "google_user", default: false
   end
 
+  add_foreign_key "dogs", "users"
 end
