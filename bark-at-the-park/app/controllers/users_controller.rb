abffa86ef594526bb67740322ace702407c7786e
jebediahelliott <jebediahelliott@gmail.com>
2019-06-26 22:42:11 -0400

    User takes nested attributes for dogs

--- bark-at-the-park/app/controllers/users_controller.rb
+++ bark-at-the-park/app/controllers/users_controller.rb
@@ -48,6 +48,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, dogs_attributes: [:name])
     end
 end
