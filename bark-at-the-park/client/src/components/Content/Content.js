94a26a3b5bc646aeab550cec54afb35f6373362c
Gonpeche <espeche.g@gmail.com>
2019-07-08 14:32:43 -0300

    nailing with flexbox

--- bark-at-the-park/client/src/components/Content/Content.js
+++ bark-at-the-park/client/src/components/Content/Content.js
@@ -32,17 +32,20 @@ export default class Content extends Component {
                         <Map />
                     </div>
                     <div className="Content__Body-Parks">
-                        {parks.map((park, i) => {
-                            return (
-                                <div key={i}>
-                                        <ul>
-                                            <li>{park.name}</li>
-                                            <li>{park.location}</li>
-                                            <li>Users checked in: {park.online}</li>
-                                        </ul>                            
-                                </div>
-                            )
-                        })}
+                        <div className="Content__Body-Parks-list">
+                            {parks.map((park, i) => {
+                                return (
+                                    <div key={i} className="Content__Body-Parks-list-park">
+                                        <h3>{park.name}</h3>
+                                        <h3>{park.location}</h3>
+                                        <h3>Users checked in: {park.online}</h3>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="Content__Body-Parks-Checkin">
+                            CHECKIN
+                        </div>
                     </div>
                 </div>
             </div>
