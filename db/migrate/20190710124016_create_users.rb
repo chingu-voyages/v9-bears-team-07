8dba689332141b16337845e945a2c89206c1eecb
jebediahelliott <jebediahelliott@gmail.com>
2019-07-10 09:05:36 -0400

    Add migrations

--- db/migrate/20190710124016_create_users.rb
+++ db/migrate/20190710124016_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string "first_name"
+      t.string "last_name"
+      t.string "email"
+      t.string "password_digest"
+      t.boolean "logged_in", default: false
+      t.boolean "google_user", default: false
+      t.string "avatar"
       t.timestamps
     end
   end
--- db/migrate/20190710125113_create_dogs.rb
+++ db/migrate/20190710125113_create_dogs.rb
@@ -1,7 +1,9 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
-
+      t.string "name"
+      t.string "avatar"
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
