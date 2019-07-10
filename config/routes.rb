188d6f9b9222614ab76aafa4b3d2c058e5654883
jebediahelliott <jebediahelliott@gmail.com>
2019-07-10 09:12:13 -0400

    Migrate config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :parks
   resources :users
+  post '/auth/google_login' => 'auth#google_login'
+  post '/auth/email_login' => 'auth#email_login'
+  patch '/checkin/:id' => 'parks#checkin'
+  patch '/checkout/:id' => 'parks#checkout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
