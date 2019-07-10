52dfee853b388aa32d54dc327c1ee42663033777
jebediahelliott <jebediahelliott@gmail.com>
2019-07-10 10:31:07 -0400

    configure for react-router

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   post '/auth/email_login' => 'auth#email_login'
   patch '/checkin/:id' => 'parks#checkin'
   patch '/checkout/:id' => 'parks#checkout'
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
