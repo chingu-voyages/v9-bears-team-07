3134e9ce76a24122940169f978b7e0c6dca408f5
jebediahelliott <jebediahelliott@gmail.com>
2019-07-15 14:23:33 -0400

    Seed some dog parks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# t.string "name"
+# t.string "address"
+# t.time "opening_time"
+# t.time "closing_time"
+# t.decimal "location", default: [], array: true
+
+parks = [
+  {
+    name: 'Tudek Dog Park',
+    address: 'Park Crest Ln, State College, PA 16803',
+    location: [40.8004715, -77.891893]
+  }
+]
+https://www.google.com/maps/search/Tudek+Dog+Park+lat+and+long/@40.8004715,-77.891893,17z/data=!3m1!4b1
